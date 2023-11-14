27d0eba3d28e7f942650ba35250565a7547cfd32
Vitor Delgado <vitor.ldelgado@gmail.com>
2023-11-14 14:39:31 -0300

    Add 2nd test route

--- src/main/java/com/task/manager/controller/TestApiController.java
+++ src/main/java/com/task/manager/controller/TestApiController.java
@@ -1,13 +1,19 @@
 package com.task.manager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestApi {
+public class TestApiController {
 
     @GetMapping("/test-api")
     private String teste() {
         return "Sucesso";
+    }
+
+    @GetMapping("/test-api-welcome")
+    public String testWelcome(@RequestParam(name = "nome") String nome) {
+        return "Hello " + nome + ", seja bem vindo!";
     }
 }
